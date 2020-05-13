8d9423223c07695bf55f849016d2ee571dfc570c
rui.ishikawa <rui.ishikawa.business@gmail.com>
2020-05-13 23:07:03 +0900

    [modify] addEventListnnerを使用、TODOを配列で管理するよう修正

--- javascript/task3-1/task3_1.js
+++ javascript/task3-1/task3_1.js
@@ -1,22 +1,58 @@
-function addTodo() {
-  const parent = document.getElementById("parent");
-  const task = document.getElementById("task");
-  const row = todoList.rows.length;
-  
-  const tr = document.createElement("tr");
-  const td1 = document.createElement("td");
-  const td2 = document.createElement("td");
-  const td3 = document.createElement("td");
-  const td4 = document.createElement("td");
-  const html3 = `<input type="button" value="作業中">`;
-  const html4 = `<input type="button" value="削除">`;
-  td1.innerText = row;
-  td2.innerText = task.value;
-  td3.innerHTML += html3;
-  td4.innerHTML += html4;
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  tr.appendChild(td3);
-  tr.appendChild(td4);
-  parent.appendChild(tr);
+function displayTodos() {
+  // 子ノードをクリア
+  parent.textContent = ''
+  const fragment = document.createDocumentFragment();
+
+  // オブジェクトを配列に追加
+  addTodo()
+
+  // 配列の要素分子ノードを追加
+  for (var i = 0; i < todos.length; i++) {
+    const tr = document.createElement("tr");
+
+    // 連番を作成
+    const td_index = document.createElement("td");
+    const val_index = document.createTextNode(i);
+    td_index.appendChild(val_index);
+    tr.appendChild(td_index);
+
+    // コメントを作成
+    const td_task = document.createElement("td");
+    const val_task = document.createTextNode(todos[i].task);
+    td_task.appendChild(val_task);
+    tr.appendChild(td_task);
+
+    // 状態ボタンを作成
+    const td_status = document.createElement("td");
+    const td_status_input = document.createElement("input");
+    td_status_input.setAttribute("type","button");
+		td_status_input.setAttribute("value",todos[i].status);
+    td_status.appendChild(td_status_input);
+    tr.appendChild(td_status);
+
+    // 削除ボタンを作成
+    const td_delete = document.createElement("td");
+    const td_delete_input = document.createElement("input");
+    td_delete_input.setAttribute("type","button");
+		td_delete_input.setAttribute("value","削除");
+    td_delete.appendChild(td_delete_input);
+    tr.appendChild(td_delete);
+
+    fragment.appendChild(tr);  
+  };
+  parent.appendChild(fragment);
 };
+
+function addTodo() {
+  const task = document.getElementById("task").value;
+  var todo = {
+    task: task,
+    status: '作業中'
+  };
+  todos.push(todo);
+};
+
+var todos = [];
+const parent = document.getElementById("parent");
+var btn = document.getElementById('btn');
+btn.addEventListener('click', displayTodos, false);
