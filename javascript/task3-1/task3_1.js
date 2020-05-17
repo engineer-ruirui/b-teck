849c8cedab13836ee174739da3213f0080e17b84
rui.ishikawa <rui.ishikawa.business@gmail.com>
2020-05-17 12:24:37 +0900

    [modify] ・削除イベントの」ハンドリングにaddEventLisnerを使うよう修正 ・statusボタン、削除ボタンの作成を別だし ・createNode から initializeNodeにメソッド名を変更

--- javascript/task3-1/task3_1.js
+++ javascript/task3-1/task3_1.js
@@ -1,20 +1,13 @@
 function displayTodos() {
   // オブジェクトを配列に追加
   addTodo();
-  // ノードを作成
-  createNode();
+  // ノードを初期化
+  initializeNode();
   // 入力フォームをクリア
   document.getElementById("task").value = "";
 };
 
-function reDisplayTodos() {
-  // オブジェクトをから削除
-  deleteTodo();
-  // ノードを作成
-  createNode();
-};
-
-function createNode() {
+function initializeNode() {
   // 子ノードをクリア
   parent.textContent = ''
   const fragment = document.createDocumentFragment();
@@ -22,40 +15,25 @@ function createNode() {
   // 配列の要素分子ノードを追加
   for (let i = 0; i < todos.length; i++) {
     const tr = document.createElement("tr");
-		tr.setAttribute("id", i);
-
     // 連番を作成
     const tdIndex = document.createElement("td");
     const valIndex = document.createTextNode(i);
     tdIndex.appendChild(valIndex);
     tr.appendChild(tdIndex);
-
     // コメントを作成
     const tdTask = document.createElement("td");
     const valTask = document.createTextNode(todos[i].task);
     tdTask.appendChild(valTask);
     tr.appendChild(tdTask);
-
     // 状態ボタンを作成
-    const tdStatus = document.createElement("td");
-    const tdStatusInput = document.createElement("input");
-    tdStatusInput.setAttribute("type","button");
-		tdStatusInput.setAttribute("value",todos[i].status);
-    tdStatus.appendChild(tdStatusInput);
-    tr.appendChild(tdStatus);
-
+    tr.appendChild(createStatusButton(i));
     // 削除ボタンを作成
-    const tdDelete = document.createElement("td");
-    const tdDeleteInput = document.createElement("input");
-    tdDeleteInput.setAttribute("type","button");
-		tdDeleteInput.setAttribute("value","削除");
-		tdDeleteInput.setAttribute("onClick","reDisplayTodos()");
-    tdDelete.appendChild(tdDeleteInput);
-    tr.appendChild(tdDelete);
-
+    tr.appendChild(createDeleteButton());
     fragment.appendChild(tr);  
   };
   parent.appendChild(fragment);
+  // 削除イベントを登録
+  registerDeleteEvent();
 };
 
 function addTodo() {
@@ -68,11 +46,46 @@ function addTodo() {
 };
 
 function deleteTodo() {
-  const itemId = event.target.parentNode.parentNode.id;
-  todos.splice(itemId,1);
+  // 指定の要素を削除
+  todos.splice(deleteIndex,1);
+  // ノードを初期化
+  initializeNode();
 };
+
+function registerDeleteEvent() {
+  const elms = document.querySelectorAll(".deleteBtn");
+  // クリックイベントをトリガーにインデックスを取得
+  elms.forEach((elm) => {
+    elm.addEventListener("click", () => {
+      // 対象要素のインデックスを取得
+      deleteIndex = [].slice.call(elms).indexOf(elm);
+      deleteTodo();
+    });
+  });
+};
+
+function createStatusButton(index) {
+  const tdStatus = document.createElement("td");
+  const tdStatusInput = document.createElement("input");
+  tdStatusInput.setAttribute("type","button");
+  tdStatusInput.setAttribute("value",todos[index].status);
+  tdStatus.appendChild(tdStatusInput);
+  return tdStatus;
+};
+
+function createDeleteButton() {
+  const tdDelete = document.createElement("td");
+  const tdDeleteInput = document.createElement("input");
+  tdDeleteInput.setAttribute("type","button");
+  tdDeleteInput.setAttribute("value","削除");
+  tdDeleteInput.setAttribute("class","deleteBtn");
+  tdDelete.appendChild(tdDeleteInput);
+  return tdDelete;
+}
 
 const todos = [];
 const parent = document.getElementById("parent");
 const btn = document.getElementById("btn");
 btn.addEventListener("click", displayTodos, false);
+// クリックした要素のインデックスを格納する変数
+let deleteIndex;
